e6c0ed4d534fbd8bccc1681f2905046a61bae8fd
Sapa <nursapa2001@gmail.com>
2024-11-08 22:46:00 +0600

    setup Nav section and divided blogs and bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+import Blogs from './components/Blogs/Blogs'
+import Header from './components/Header/Header'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 className='text-6xl bg-black font-bold text-white'>react</h1>
+    <>
+      <Header></Header>
+      <div className='md:flex container mx-auto'>
+          <Blogs></Blogs>
+          <Bookmarks></Bookmarks>
+      </div>
+    </>
   )
 }
 
